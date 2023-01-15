e47c545d5156c384850308dbba6e49743a88a1ec
Filip <filip.knias@o2.pl>
2023-01-15 15:19:20 +0100

    create get vikings method

---
+++ types/vikings.ts
@@ -0,0 +1,9 @@
+export type Viking = {
+  fullName: string;
+  presenceOfChildren: boolean;
+  age: number;
+  hometown: string;
+  canFightWithSword: boolean;
+  canFightWithAxe: boolean;
+  canFightWithSpear: boolean;
+};
