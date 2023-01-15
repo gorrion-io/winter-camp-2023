1141914b8362459c6a9ce4f171765087992cc737
patrykkubala <patrykkubala1999@gmail.com>
2023-01-15 11:30:11 +0100

    test

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = {
-  nextConfig,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.yaml$/,
-      use: "js-yaml-loader",
-    });
-
-    return config;
-  },
-};
+module.exports = nextConfig
